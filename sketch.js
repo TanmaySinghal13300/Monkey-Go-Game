4832128d7158bb99b96221479c642b4261313377
TanmaySinghal13300 <72397317+TanmaySinghal13300@users.noreply.github.com>
2020-10-05 19:58:53 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,7 +19,7 @@ function preload(){
 
 
 function setup() {
-createCanvas(900,400);  
+createCanvas(500,400);  
 FoodGroup=new Group();
 obstacleGroup=new Group();
 monkey=createSprite(50,190,20,20);
